55265bb2121f4d2813872e205d4ff7171a0ebf4e
Fran Jurinec <franjurinec@live.com>
2022-06-14 18:09:23 +0000

    Exercise 1.12

--- Project/app.js
+++ Project/app.js
@@ -1,13 +1,46 @@
 const express = require('express')
+const fs = require('fs')
+const ImageDownloader = require('image-downloader')
 
+// Static local storage location
+const basepath = '/usr/src/app/files'
+
+// Express Setup
 const app = express()
-
 const port = process.env.PORT ?? 3000
-
+app.use(express.static(basepath))
 app.set('view engine', 'pug')
 
-app.get('/', (req, res) => {
+// Load daily image metadata
+let imageMeta = {}
+try {
+    imageMeta = JSON.parse(fs.readFileSync(basepath + 'imageMeta.json'))
+} catch(err) {
+    console.log(err)
+    console.log('Found no existing metadata.')
+}
+
+// Download new image and update stored metadata
+async function updateImage() {
+    // Guard clause - stop if image is up-to-date
+    if (new Date().toDateString() === imageMeta.date) return
+
+    await ImageDownloader.image({
+        url: 'https://picsum.photos/1024/512',
+        dest: basepath + 'daily.jpg'
+    })
+    
+    imageMeta.date = new Date().toDateString()
+    fs.writeFileSync(basepath + 'imageMeta.json', JSON.stringify(imageMeta))
+}
+
+// Routes
+app.get('/', async (req, res) => {
+    await updateImage()
     res.render('index')
 })
 
+app.get('/kill', () => process.exit(5))
+
+// Start Listening
 app.listen(port, () => console.log(`Server started on port ${port}`))
