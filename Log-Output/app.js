906673e268525f772b541b1dd0525602b91a56d9
Fran Jurinec <franjurinec@live.com>
2022-06-23 13:53:13 +0200

    Exercise 2.06

--- Log-Output/app.js
+++ Log-Output/app.js
@@ -1,16 +1,20 @@
+require('dotenv').config()
 const crypto = require('crypto')
 const express = require('express')
 
 const app = express()
 const port = process.env.PORT ?? 3000
 
-let hash = crypto.randomUUID()
+const helloMessage = process.env.MESSAGE ?? 'No message specified.'
+const hash = crypto.randomUUID()
 
 setInterval(() => console.log(`${new Date().toISOString()}: ${hash}`), 5000)
 
 app.get('/', async (_, res) => {
-    let pingPongData = await fetch('http://ping-pong-svc/count').then(res => res.json())
-    res.send(`${new Date().toISOString()}: ${hash} <br> Ping / Pongs: ${pingPongData.count}`)
+    let pingPongData = await fetch('http://ping-pong-svc/count').then(res => res.json()).catch(() => 0)
+    res.send(`${helloMessage}
+                <br> ${new Date().toISOString()}: ${hash} 
+                <br> Ping / Pongs: ${pingPongData.count ?? 0}`)
 })
 
 app.listen(port)
